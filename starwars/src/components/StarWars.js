adc7667906f7e69fa4a749257d51290e28b9a1ad
Victor Sanchez <v08311996@gmail.com>
2020-01-10 20:56:57 -0500

    Updated some changes

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,6 +1,7 @@
-import axios from "axios";
-import StarWarsList from "./components/StarWarsList";
 import React, { useState, useEffect} from "react";
+import axios from "axios";
+import StarWarsList from "./StarWarsList";
+
 
 
     function StarWars() {
@@ -23,20 +24,20 @@ import React, { useState, useEffect} from "react";
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch peoples from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return(
     <div className = "names">
-        {Comp1.map((character)=>{
+        {Comp1.map((people)=>{
             return(
                 <StarWarsList
-                    charName = {character.name}
-                    gender= {character.gender}
-                    height = {character.height}
-                    key = {character.id}
-                    hair_color = {character.hair_color}
+                    charName = {people.name}
+                    gender= {people.gender}
+                    height = {people.height}
+                    key = {people.id}
+                    hair_color = {people.hair_color}
                 />
             )}
         )}
--- starwars/src/components/StarWarsList.js
+++ starwars/src/components/StarWarsList.js
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from "react";
-import styled from 'styled-components'
+import React from "react";
+import styled from 'styled-components';
 
 const StyledDiv = styled.div``;
 
-// paragraph
 const StyledP = styled.p`
 opacity: 1
 color: white;
 font-weight: bold;
 `;
 
-// section
+
 const StyledSection = styled.section`
 display: inline-block;
 padding: 3rem;
@@ -21,7 +20,7 @@ margin-bottom: 20px;
 border: 2px dashed white;
 `;
 
-// headers h1 - h6
+
 const StyledHead = styled.h2`
     font-size: 1.35rem;
     max-width: 100%;
@@ -32,11 +31,7 @@ const StyledHead = styled.h2`
     }
 `;
 
-// a
-const StyledA = styled.a``;
-export function CharacterListing(props) {
-
-    if (!props.person) return <h3>Loading...</h3>;
+export function StarWarsList (props) {
 
     return (
         <StyledDiv className="App">
@@ -47,7 +42,8 @@ export function CharacterListing(props) {
                 <StyledP>{props.person.gender}</StyledP>
             </StyledSection>
         </StyledDiv>
-    );
-}
+    )
+};
+
 
 export default StarWarsList;
